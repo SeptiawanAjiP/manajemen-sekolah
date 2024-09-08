67344170ff010a52e11df5ba654a86786443e1bd
septiawanajipradana <septiawanajipradana@gmail.com>
2024-09-08 13:15:30 +0700

    chore: first init

---
+++ database/migrations/2024_09_08_055906_create_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('student_number')->unique();
+            $table->date('date_of_birth');
+            $table->foreignId('grade_id')->nullable()->constrained('grades')->onDelete('set null');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
